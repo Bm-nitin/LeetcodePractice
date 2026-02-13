b57caf4af74dfa40ddaaa9443587d89d90f40f5e
Nitin Kumar <nitinkumar.2024@lpu.in>
2026-02-13 23:35:54 +0530

    Added solution for LeetCode problem 507 (Perfect Number)

---
+++ leet507.cpp
@@ -0,0 +1,33 @@
+// leetcode 507. Perfect Number
+// A perfect number is a positive integer that is equal to the sum of its positive divisors
+// excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+// Given an integer n, return true if n is a perfect number, otherwise return false.
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num<=1)return false;
+        int sum=1;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+                if(i*i!=num)sum+=num/i;
+            }
+        }
+        return sum==num;
+    }
+};
+// java code for same
+/*
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num<=1)return false;
+        int sum=1;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+                if(i*i!=num)sum+=num/i;
+            }
+        }
+        return sum==num;
+    }
+}*/
